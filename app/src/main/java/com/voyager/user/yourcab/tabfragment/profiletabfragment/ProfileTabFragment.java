c05b5c80eae013f0692b5af6171a1fb0337ba05b
rimon <rimon@tigerinnovations.org>
2017-09-13 21:19:01 +0530

    Working Ui for the Fragment.

--- app/src/main/java/com/voyager/user/yourcab/tabfragment/profiletabfragment/ProfileTabFragment.java
+++ app/src/main/java/com/voyager/user/yourcab/tabfragment/profiletabfragment/ProfileTabFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.voyager.user.yourcab.R;
 import com.voyager.user.yourcab.tabfragment.hometabfragment.model.MapDetails;
@@ -17,11 +19,20 @@ import java.util.List;
  * Created by User on 9/8/2017.
  */
 
-public class ProfileTabFragment  extends Fragment {
+public class ProfileTabFragment  extends Fragment implements View.OnClickListener{
 
 
     List<MapDetails> mapDetailsList;
     String yourFilePath ;
+
+    TextView driverProfile;
+    TextView profileHelp;
+    TextView driverDoc;
+    TextView driverSetting;
+    TextView driverAbout;
+    TextView editDriverCarImg;
+    TextView editDriverImg;
+    Button btnSignOut;
 
 
     private Activity activity;
@@ -41,7 +52,23 @@ public class ProfileTabFragment  extends Fragment {
         setHasOptionsMenu(true);
         View rootView = inflater.inflate(R.layout.profile_tab_fragment, container, false);
         System.out.println("ProfileTabFragment");
+        driverProfile = (TextView) rootView.findViewById(R.id.driverProfile);
+        profileHelp = (TextView) rootView.findViewById(R.id.profileHelp);
+        driverDoc = (TextView) rootView.findViewById(R.id.driverDoc);
+        driverSetting = (TextView) rootView.findViewById(R.id.driverSetting);
+        driverAbout = (TextView) rootView.findViewById(R.id.driverAbout);
+        editDriverCarImg = (TextView) rootView.findViewById(R.id.driverAbout);
+        editDriverImg = (TextView) rootView.findViewById(R.id.driverAbout);
+        btnSignOut = (Button) rootView.findViewById(R.id.btnSignOut);
 
+        driverProfile.setOnClickListener(this);
+        profileHelp.setOnClickListener(this);
+        driverDoc.setOnClickListener(this);
+        driverSetting.setOnClickListener(this);
+        driverAbout.setOnClickListener(this);
+        btnSignOut.setOnClickListener(this);
+        editDriverCarImg.setOnClickListener(this);
+        editDriverImg.setOnClickListener(this);
 
         return rootView;
     }
@@ -93,5 +120,26 @@ public class ProfileTabFragment  extends Fragment {
     }
 
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.driverProfile:
+               break;
+            case R.id.profileHelp:
+                break;
+            case R.id.driverDoc:
+                break;
+            case R.id.driverSetting:
+                break;
+            case R.id.driverAbout:
+                break;
+            case R.id.btnSignOut:
+                break;
+            case R.id.editDriverCarImg:
+                break;
+            case R.id.editDriverImg:
+                break;
 
+        }
+    }
 }
